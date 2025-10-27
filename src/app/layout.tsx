0f5db5fc9914d419d17cb07d70b5cb8b65fc6653
ramong26 <rlatndus21049@gmail.com>
2025-10-27 09:28:26 +0900

    feat/google-analytics - 구글 분석

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import QueryProvider from './providers/QueryProvider';
 import { Archivo_Black, Oswald } from 'next/font/google';
@@ -30,6 +31,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-TQ448MNL7R"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TQ448MNL7R');
+          `}
+        </Script>
+      </head>
       <body className={`${oswald.variable} ${archivoBlack.variable}`}>
         <HeaderMain />
         <QueryProvider>{children}</QueryProvider>
