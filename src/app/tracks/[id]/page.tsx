98ef6b38c76f713465fdd28d07aaaae5480945dd
ramong26 <rlatndus21049@gmail.com>
2026-01-09 12:00:31 +0900

    fix: track페이지 TrackComments 제거

--- src/app/tracks/[id]/page.tsx
+++ src/app/tracks/[id]/page.tsx
@@ -1,4 +1,4 @@
-// import { getBaseUrl } from '@/lib/utils/baseUrl';
+import { getBaseUrl } from '@/lib/utils/baseUrl';
 import TrackDescription from '@/features/tracks/components/TrackDescription/TrackDescription';
 
 import TrackClient from '@/features/tracks/components/TrackClient';
@@ -15,10 +15,11 @@ interface PageProps {
 }
 
 export default async function TrackPage({ params }: PageProps) {
-  // const baseUrl = getBaseUrl();
+  const baseUrl = getBaseUrl();
+  console.log('Base URL:', baseUrl);
   const { id } = await params;
 
-  const res = await fetch(`/api/tracks/${id}`, {
+  const res = await fetch(`${baseUrl}/api/tracks/${id}`, {
     cache: 'no-store',
   });
 
